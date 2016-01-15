caee1244e70fa9032b549f239038b06b6759f765
cory <whippoorwill.in.flight@gmail.com>
2016-01-15 12:56:11 -0800

    add nothing that works

--- yourvacation/js/app.js
+++ yourvacation/js/app.js
@@ -1,4 +1,14 @@
-$('#blanks form').submit(function(event) {
+$(document).ready(function(){
+  $('blanks form').submit(function(event) {
+    var q1 = $("input#question1").val();
+    var q2 = $("input#question2").val();
+    var q3 = $("input#question3").val();
+    var q4 = $("input#question4").val();
+    var q5 = $("input#question5").val();
+
+    event.preventDefault();
+  });
+});
 
 
 
@@ -6,15 +16,17 @@ $('#blanks form').submit(function(event) {
 
 
 
-  var question1Input = $("input#question1").val();
-  var question2Input = $("input#question2").val();
-  var question3Input = $("input#question3").val();
-  var question4Input = $("input#question1question4").val();
-  var question5Input = $("input#question5").val();
-$('.question1').text(question1Input);
-$('.question2').text(question2Input);
-$('.question3').text(question3Input);
-$('.question4').text(question4Input);
-$('.question5').text(question5Input);
+  //var q1 = $('#form').serializeArray()
 
-})
+//  var question2Input = $("input#question2").val();
+//  var question3Input = $("input#question3").val();
+//  var question4Input = $("input#question1question4").val();
+//  var question5Input = $("input#question5").val();
+//$('.question2').text(question2Input);
+//$('.question3').text(question3Input);
+//$('.question4').text(question4Input);
+//$('.question5').text(question5Input);
+
+//});
+
+//var choices = [$('#form').serializeArray()]
